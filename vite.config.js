e4a3f182f03b3c462885bf38415bbbf8ab80e93e
MD Dehan <mddehan164@gmail.com>
2025-10-23 20:58:59 +0600

    fixing backend path

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://my-photos-backend.onrender.com",
         changeOrigin: true,
       },
     },
